9bc2ecf659e261ed3ee72e08ec3b2b217bc8d53f
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-15 21:31:11 +0400

    feat: entity design for Author, Book, Member with relations

---
+++ library/src/main/java/org/ironhack/project/library/entity/Author.java
@@ -0,0 +1,34 @@
+package org.ironhack.project.library.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Book> books = new ArrayList<>();
+}
