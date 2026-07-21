8821c2bba62177f5e9543c381d7e96004d6f41e8
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 17:07:57 +0400

    feat: add BookService

--- src/main/java/org/ironhack/project/library/service/BookService.java
+++ src/main/java/org/ironhack/project/library/service/BookService.java
@@ -1,4 +1,45 @@
 package org.ironhack.project.library.service;
 
+import lombok.RequiredArgsConstructor;
+import org.ironhack.project.library.dto.response.BookResponse;
+import org.ironhack.project.library.entity.Book;
+import org.ironhack.project.library.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public List<BookResponse> getAllBooks() {
+
+        return bookRepository.findAll()
+                .stream()
+                .map(book -> new BookResponse(
+                        book.getId(),
+                        book.getTitle(),
+                        book.getIsbn(),
+                        book.getAuthor().getId(),
+                        book.getAuthor().getName()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    public BookResponse getBookById(Long id) {
+
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+
+        return new BookResponse(
+                book.getId(),
+                book.getTitle(),
+                book.getIsbn(),
+                book.getAuthor().getId(),
+                book.getAuthor().getName()
+        );
+    }
 }
