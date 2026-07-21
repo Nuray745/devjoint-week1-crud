95cd7b4ee1fc7769872415f745331d653df11934
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 13:40:06 +0400

    feat: add MemberRequest DTO

--- src/main/java/org/ironhack/project/library/dto/request/MemberRequest.java
+++ src/main/java/org/ironhack/project/library/dto/request/MemberRequest.java
@@ -1,4 +1,21 @@
 package org.ironhack.project.library.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberRequest {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
 }
