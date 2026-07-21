0a06f8410ce6cbb24b69ae13b0b6223e9eca271c
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 13:39:09 +0400

    feat: add BookRequest DTO

--- src/main/java/org/ironhack/project/library/dto/request/BookRequest.java
+++ src/main/java/org/ironhack/project/library/dto/request/BookRequest.java
@@ -1,4 +1,22 @@
 package org.ironhack.project.library.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookRequest {
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String isbn;
+
+    private Long authorId;
 }
