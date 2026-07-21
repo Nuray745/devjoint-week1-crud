f7d78ba54d48d9ce83a4c76e53fd4af05a57e800
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 17:11:03 +0400

    feat: add MemberService

--- src/main/java/org/ironhack/project/library/service/MemberService.java
+++ src/main/java/org/ironhack/project/library/service/MemberService.java
@@ -1,4 +1,41 @@
 package org.ironhack.project.library.service;
 
+import lombok.RequiredArgsConstructor;
+import org.ironhack.project.library.dto.response.MemberResponse;
+import org.ironhack.project.library.entity.Member;
+import org.ironhack.project.library.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public List<MemberResponse> getAllMembers() {
+
+        return memberRepository.findAll()
+                .stream()
+                .map(member -> new MemberResponse(
+                        member.getId(),
+                        member.getName(),
+                        member.getEmail()
+                ))
+                .collect(Collectors.toList());
+    }
+
+    public MemberResponse getMemberById(Long id) {
+
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        return new MemberResponse(
+                member.getId(),
+                member.getName(),
+                member.getEmail()
+        );
+    }
 }
