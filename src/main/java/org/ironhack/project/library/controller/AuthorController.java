9d49f5d93f3e87e6ada265d6116a95adbe15eac1
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 18:07:19 +0400

    feat: add CRUD endpoints to AuthorController

--- src/main/java/org/ironhack/project/library/controller/AuthorController.java
+++ src/main/java/org/ironhack/project/library/controller/AuthorController.java
@@ -1,13 +1,13 @@
 package org.ironhack.project.library.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.ironhack.project.library.dto.request.AuthorRequest;
 import org.ironhack.project.library.dto.response.AuthorResponse;
 import org.ironhack.project.library.service.AuthorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,12 +19,36 @@ public class AuthorController {
     private final AuthorService authorService;
 
     @GetMapping
-    public List<AuthorResponse> getAllAuthors() {
-        return authorService.getAllAuthors();
+    public ResponseEntity<List<AuthorResponse>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAllAuthors());
     }
 
     @GetMapping("/{id}")
-    public AuthorResponse getAuthorById(@PathVariable Long id) {
-        return authorService.getAuthorById(id);
+    public ResponseEntity<AuthorResponse> getAuthorById(@PathVariable Long id) {
+        return ResponseEntity.ok(authorService.getAuthorById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthorResponse> createAuthor(
+            @Valid @RequestBody AuthorRequest request) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(authorService.createAuthor(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AuthorResponse> updateAuthor(
+            @PathVariable Long id,
+            @Valid @RequestBody AuthorRequest request) {
+
+        return ResponseEntity.ok(authorService.updateAuthor(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+
+        authorService.deleteAuthor(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
