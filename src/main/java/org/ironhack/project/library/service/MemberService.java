aa9d4eaef89f09a6fb32b564779f768432927c28
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 17:28:20 +0400

    refactor: use MemberMapper in MemberService

--- src/main/java/org/ironhack/project/library/service/MemberService.java
+++ src/main/java/org/ironhack/project/library/service/MemberService.java
@@ -3,6 +3,7 @@ package org.ironhack.project.library.service;
 import lombok.RequiredArgsConstructor;
 import org.ironhack.project.library.dto.response.MemberResponse;
 import org.ironhack.project.library.entity.Member;
+import org.ironhack.project.library.mapper.MemberMapper;
 import org.ironhack.project.library.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +17,9 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public List<MemberResponse> getAllMembers() {
-
         return memberRepository.findAll()
                 .stream()
-                .map(member -> new MemberResponse(
-                        member.getId(),
-                        member.getName(),
-                        member.getEmail()
-                ))
+                .map(MemberMapper::toResponse)
                 .collect(Collectors.toList());
     }
 
@@ -32,10 +28,6 @@ public class MemberService {
         Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Member not found"));
 
-        return new MemberResponse(
-                member.getId(),
-                member.getName(),
-                member.getEmail()
-        );
+        return MemberMapper.toResponse(member);
     }
 }
