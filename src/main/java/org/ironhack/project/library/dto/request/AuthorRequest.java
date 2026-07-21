5c766cf807ba1d59af22d9a3d969b967a637e26d
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-21 13:36:26 +0400

    feat: add AuthorRequest DTO

---
+++ src/main/java/org/ironhack/project/library/dto/request/AuthorRequest.java
@@ -0,0 +1,19 @@
+package org.ironhack.project.library.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorRequest {
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+}
