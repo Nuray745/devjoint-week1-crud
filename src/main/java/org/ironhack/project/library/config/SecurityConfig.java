bfcf12a57534fcb7d528bbec8b5abde1e510f1e5
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-08-15 17:16:52 +0400

    refactor: SecurityConfig

--- src/main/java/org/ironhack/project/library/config/SecurityConfig.java
+++ src/main/java/org/ironhack/project/library/config/SecurityConfig.java
@@ -55,6 +55,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml"
+                        ).permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/authors/**", "/books/**", "/members/**").hasAnyRole("USER", "ADMIN")
